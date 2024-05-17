b8e8203234eb490d79a116c47090fe000a1bf409
Robert MacHunter <r.machunter@uqconnect.edu.au>
2024-05-18 08:30:19 +1000

    Made file read a part of command line arguements

--- ai_model/ABS_mind.py
+++ ai_model/ABS_mind.py
@@ -1,19 +1,31 @@
 from sklearn.kernel_ridge import KernelRidge
+from sklearn.neighbors import NearestNeighbors
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import mean_squared_error
 from sklearn.model_selection import train_test_split
 from sklearn.datasets import make_regression
 import numpy as np
 import pandas as pd
+import sys
 
-def main(df: pd.DataFrame):
+def main():
     
+    df = pd.read_csv(sys.argv[1])
+
     print(f"{df.head()}\n")
     print(f"{df.info()}\n")
     print(f"{df.isnull().sum()}\n")
 
-    y = df['class']
-    X = df[1:, '0':]
+    y = df['abilities']
+    X = df.loc[:, 'against_bug':]
+
+    print("y is: \n", y)
+    print("X is: \n", X)
+
+    if(input("Continue? ") != 'y'):
+        print("Exiting...")
+        return
+
     # Generate synthetic data for demonstration
     # X, y = make_regression(n_samples=100, n_features=1, noise=0.1, random_state=42)
 
@@ -30,16 +42,28 @@ def main(df: pd.DataFrame):
 
     krr_model = KernelRidge(alpha=alpha, kernel=kernel, gamma=gamma)
 
+    knn_model = NearestNeighbors(n_neighbors=3)
+
     # Train the model
     krr_model.fit(X_train, y_train)
+
+    knn_model.fit(X_train, y_train)
 
     # Predict on the test set
     y_pred = krr_model.predict(X_test)
     print("Actual: ", y_test, "Predicted: ", krr_model.predict(X_test))
 
+    knn_y_pred = knn_model.kneighbors(X_test)
+
+    distances, indices = mean_squared_error(y_test, knn_y_pred)
+
+    #print("KNN MSE: " )
+
     # Calculate Mean Squared Error
     mse = mean_squared_error(y_test, y_pred)
     print("Mean Squared Error:", mse)
+
+
 
 
 
