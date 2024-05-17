0e16f5e3e9d6dd314fdc446ad2e85033c0b1bd21
Luke93ss <106220745+Luke93ss@users.noreply.github.com>
2024-05-17 22:57:50 +1000

    Add files via upload

---
+++ microphone/include/audio_service.h
@@ -0,0 +1,41 @@
+#ifndef AUDIO_SERVICE_H_
+#define AUDIO_SERVICE_H_
+#include <zephyr/types.h>
+#include <stddef.h>
+#include <string.h>
+#include <errno.h>
+#include <zephyr/sys/printk.h>
+#include <zephyr/sys/byteorder.h>
+#include <zephyr/kernel.h>
+
+#include <zephyr/bluetooth/bluetooth.h>
+#include <zephyr/bluetooth/hci.h>
+#include <zephyr/bluetooth/conn.h>
+#include <zephyr/bluetooth/uuid.h>
+#include <zephyr/bluetooth/gatt.h>
+#include <zephyr/bluetooth/services/hrs.h>
+#include <stdint.h>
+#include <zephyr/sys/util.h>
+#include <zephyr/bluetooth/byteorder.h>
+
+/* Define UUID for Audio Service and Characteristic */
+#define BT_UUID_AUDIO_VAL 0x1798
+#define BT_UUID_AUDIO \
+        BT_UUID_DECLARE_16(BT_UUID_AUDIO_VAL)
+#define BT_UUID_AUDIO_DATA_VAL 0x1799
+#define BT_UUID_AUDIO_DATA \
+        BT_UUID_DECLARE_16(BT_UUID_AUDIO_DATA_VAL)
+
+/** @brief Initialize the Audio Service. */
+void audio_service_init(void);
+
+/** @brief Initialize Bluetooth audio. */
+int bluetooth_audio_init(void);
+
+/** @brief Send a 16-bit PCM audio value. */
+void audio_notify(uint16_t pcm_value);
+// extern void audio_notify(uint16_t* buffer);
+
+
+
+#endif /* AUDIO_SERVICE_H_ */
---
+++ microphone/include/bluetooth_audio.h
@@ -0,0 +1,7 @@
+#ifndef BLUETOOTH_AUDIO_H_
+#define BLUETOOTH_AUDIO_H_
+
+// bluetooth audio functions
+// void audio_notify(uint16_t* buffer);
+int bluetooth_audio_init(void);
+#endif
